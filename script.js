3dc6a6596e277db63c940a16797a4955575afd61
Khalenov Birzhan <birzanhalenov1@gmail.com>
2025-09-18 04:19:51 +0500

    Add thirt API

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log('Привет!')
 
 const API_1 :string = 'https://api-1.ru'
-const API_1 :string = 'https://api-2.ru'
+const API_2 :string = 'https://api-2.ru'
+const API_3 :string = 'https://api-3.ru'
